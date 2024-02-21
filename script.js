65baf25742f3b6090eeacf143ca0ccd3c4186a6b
RajkamalPippera <154339501+RajkamalPippera@users.noreply.github.com>
2024-02-22 00:01:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button")
+let marks;
+btn.addEventListener("click",()=>{
+    marks = document.querySelector("#inputvalue").value
+    if(marks>=35){
+        document.querySelector("h1").textContent = `${" 🎉Congratulations You are Pass😍"}`
+        document.querySelector("h1").style.backgroundColor = "green"
+    }
+    else if(marks<35){
+        document.querySelector("h1").textContent = `${" Fail😞"}`
+        document.querySelector("h1").style.backgroundColor = "red"
+    }
+    if(marks == ""){
+        document.querySelector("h1").textContent = alert("Please enter marks 😊")
+        document.querySelector("h1").style.backgroundColor = "white"
+    }
+    if(marks <0){
+        document.querySelector("h1").textContent = `Invaild input try again`
+    }
+})
